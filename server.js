437ba818f75b0890c5775c1b266b34507d9b7a20
soumya <twinklesou23@gmail.com>
2025-01-01 21:43:13 +0530

    demo graphql

--- server.js
+++ server.js
@@ -1,29 +1,61 @@
-var {graphql,buildSchema} = require("graphql");
-//const { Source } = require('graphql');
+// var {graphql,buildSchema} = require("graphql");
+// //const { Source } = require('graphql');
 
 
-// Construct Schema using graphQl query language
-var schema =  buildSchema
-(` 
-    type Query{
-      hello : String,
-      name: String
-    }
+// // Construct Schema using graphQl query language
+// var schema =  buildSchema
+// (` 
+//     type Query{
+//       hello : String,
+//       name: String
+//     }
+// `)
+
+// //The rootvalue provides a resolver function for each API endpoint
+// var rootValue = {
+//     hello(){
+//         return "hello world"
+//     }
+// }
+
+// //run graphql query
+// graphql({
+//     schema,
+//     source:"{ hello }",
+//     rootValue
+// }).then(response =>{
+//     console.log(response)
+// })
+
+var express = require("express")
+var { createHandler } = require("graphql-http/lib/use/express")
+var { buildSchema } = require("graphql")
+ 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
 `)
-
-//The rootvalue provides a resolver function for each API endpoint
-var rootValue = {
-    hello(){
-        return "hello world"
-    }
+ 
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello() {
+    return "Hello worldsss!"
+  },
 }
-
-//run graphql query
-graphql({
-    schema,
-    source:"{ hello }",
-    rootValue
-}).then(response =>{
-    console.log(response)
-})
-
+ 
+var app = express()
+ 
+// Create and use the GraphQL handler.
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    rootValue: root
+  })
+)
+ 
+// Start the server at port
+app.listen(4000)
+console.log("Running a GraphQL API server at http://localhost:4000/graphql")
